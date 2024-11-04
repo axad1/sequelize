d22647cda78678f75bb55fbcdaa425ca0479d66b
AxAd <asadhusn007@gmail.com>
2024-11-05 01:26:26 +0500

    enhance

--- db.js
+++
@@ -1,2 +0,0 @@
-const { Sequelize } = require("sequelize");
-module.exports = new Sequelize("sqlite:mydb.sqlite");
--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db");
+const sequelize = require(".");
 
 const User = sequelize.define(
   "users",
